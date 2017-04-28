2d619669ef62981271b0edfce25e55e03a0110df
Juan Javier Triff Cabanas <jjtriff@gmail.com>
2017-04-28 15:19:33 -0400

    correct the bug where the function manages atts from another objetc

--- lib/bookitClient.php
+++ lib/bookitClient.php
@@ -1,16 +1,23 @@
 <?php
 namespace CGHAB\BookititClient;
 
+/**
+*   Deletes an event based in its id and returns true o success.
+*   Sends an exception if anything goes wrogn.
+*/
+
+
 function deleteEvent($eventId){
     $oRestClient = new CRestClient();
     $result = $oRestClient->deleteEvent('json',false,$eventId);
 
     $oresult = json_decode($result);
     if($oresult->event->status == "true"){
-        unset($this->prereservations[$key]);
+        return true;
     }
     else {
         error_log("Error from bookitit: " . $oresult->event->id . " ". $oresult->event->message);
         throw new Exception("Error deleting date $id from day $this->date, we will retry $retries more time(s)");
     }
+
 }
