7bb48c3eb6a9f1a77eded2ada61ad9f756c27f35
Juan Javier Triff Cabanas <jjtriff@gmail.com>
2017-04-28 11:13:31 -0400

    implement funcition preserve

--- lib/bookitDay.php
+++ lib/bookitDay.php
@@ -1,6 +1,8 @@
 <?php
 
 include('bookit/CRestClient.php');
+include_once('reservationUser.php');
+include_once('jsoncollection.php');
 
 //
 //
@@ -21,8 +23,98 @@ class BookitDay extends StdClass
        $this->date = $date;
     }
 
-    public function prereserve($agenda, $service, $mail, $name)
+    function mapper(StdClass $bookitDay){
+      $this->date = $bookitDay->date;
+      $this->exec_count = $bookitDay->exec_count;
+      // esta sera una lista de los id de los turnos preservados 
+      $this->prereservations = $bookitDay->prereservations;
+    }
+
+    public function prereserve($agenda, $service, $client, $retries)
     {
+      $oRestClient = new CRestClient();
+
+      // $date = ($date == null) ? $client->proposed_date : $date ;
+      // $time = ($time == null) ? $client->proposed_time : $time ;
+
+      // repetir este ciclo hasta que se cumplan los retries
+      while ($retries >= 0) {
         
+        try
+        {
+        //buscar los huecos libres del dia de hoy
+          $sReturn = $oRestClient->getFreeSlots($service,
+          $agenda, $this->date, "json", false);
+
+          $hours = json_decode($sReturn);
+          if($hours->slots->status == "true"){
+            $hours = $hours->slots->hours;
+          }
+          else{
+            throw new Exception("An error occurred when communicating with Bookitit");
+          }
+
+          if(count($hours) == 0)
+            throw new Exception("We ran out of free slots for day $this->date", 1);
+
+          $client->dni = null;
+          foreach ($hours as $time) {
+              $sReturn = $oRestClient->addEvent( $agenda,
+                $service,
+                $this->date,
+                $this->date,
+                $oRestClient->renderMinutes($time),
+                $oRestClient->renderMinutes(date('H:i', strtotime("$time +1 hour"))),
+                "",
+                "",
+                $client->comment,
+                "",
+                "",
+                "",
+                "",
+                $client->phone,
+                "",
+                $client->name,
+                $client->phone,
+                $client->mail,
+                "json",
+                false,
+                ["document" => $client->dni]);
+
+              $oObject = json_decode($sReturn);
+              if($oObject->event->status == "true"){
+                $this->prereservations[] = $oObject->event->id;
+              }
+              else{
+                throw new Exception("An error when trying to prereserve on day $this->date, hour $time");
+              }
+
+          }
+        } catch (Exception $e){
+          $retries--;
+        }
+      }
     }
 }
+
+
+// tests
+// $d  = new BookitDay("2017-10-17");
+// $u = new ReservationUser("jj","jj@mail", "78686868", "no tomar en cuenta, solo pruebas de prereservas");
+
+// $d->prereserve("bkt103664", "bkt219175", $u, 1 );
+
+$db = new JsonCollection();
+$d = unserialize( $db->col["2017-10-17"]);
+// $nd = new BookitDay($d->date);
+// $nd->mapper($d);
+
+// $db->col[$nd->date] = serialize($nd);
+
+$d-
+
+
+
+print $d->date;
+
+
