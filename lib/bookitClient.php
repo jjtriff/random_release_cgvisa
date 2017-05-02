9a52323a532b2ffaacd319e89590ffd9ac65a32a
Juan Javier Triff Cabanas <jjtriff@gmail.com>
2017-05-02 11:16:31 -0400

    add method to get every event in one day

--- lib/bookitClient.php
+++ lib/bookitClient.php
@@ -23,3 +23,31 @@ function deleteEvent($eventId){
     }
 
 }
+
+/**
+* @param $date in the format "YYYY-MM-DD"
+*/
+
+function getDateEvents($date)
+{
+    $oRestClient = new \CRestClient();
+    $ret = $GLOBALS['free.ini']['retries'];
+    while ($ret >= 0) {
+        $result = $oRestClient->getEvents($date,
+        $date,
+        'json',
+        false);
+        
+        $oresult = json_decode($result);
+        if($oresult->events->status == "true"){
+            unset($oresult->events->status);
+            return $oresult->events;
+        }
+        else
+        {
+            error_log("Error from bookitit: " . $oresult->events->id . " ". $oresult->events->message);
+            $ret--;
+        }
+    }
+    throw new \Exception("Error getting events for date $date");
+}
