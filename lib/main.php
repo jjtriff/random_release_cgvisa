51f16bc9390f7232754f62ff083f546209e473f0
Juan Javier Triff Cabanas <jjtriff@gmail.com>
2017-05-02 12:46:51 -0400

    add a lib to keep the methods used from the main script

---
+++ lib/main.php
@@ -0,0 +1,13 @@
+<?php
+
+function print_initial_configs($ini_array){
+    // error_log("Capacidad del departamento de visado es: $ini_array[dept_capacity] personas diarias");
+    // $ob = $ini_array['overbooking_percentage'] * 100;
+    // error_log("Se trabajar'a con un overbooking de: $ob %");
+    // $cd = $ini_array['closest_days_percentage'] * 100;
+    // error_log("Para los pr'oximos 15 d'ias se abrir'an un $cp % del total de las citas");
+    // error_log("Los porcientos ")
+    error_log(print_r($ini_array));
+
+}
+
