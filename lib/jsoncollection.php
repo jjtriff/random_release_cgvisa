e50984012d8121f4ee704954d292cc669f64f920
Juan Javier Triff Cabanas <jjtriff@gmail.com>
2017-05-02 12:46:02 -0400

    add methods to encapsulate work with BookitDay types

--- lib/jsoncollection.php
+++ lib/jsoncollection.php
@@ -1,5 +1,7 @@
 <?php
 
+include_once('bookitDay.php');
+
 /**
  * class for handling the variables into json, so we can keep a local db
  */
@@ -20,6 +22,21 @@ class JsonCollection
     {
         file_put_contents($this->name, json_encode($this->col, JSON_PRETTY_PRINT));
     }
+
+    public function addDay(BookitDay $day)
+    {
+        $this->col[$day->date] = serialize($day);
+    }
+
+    public function getDay(string $date)
+    {   
+        $_ret = $this->col[$date];
+        if($_ret){
+            return unserialize($_ret);
+        }
+
+        return $_ret;
+    }
 }
 
 
@@ -32,4 +49,14 @@ class JsonCollection
 
 // $db->col['DS'] = $i;
 
+// $db = new JsonCollection();
+// // $i = new BookitDay('2017-10-15');
+// // $db->addDay($i);
+// $i = $db->getDay('2017-10-14');
+// // $i->fecha = '2017-05-06';
+// // $i->abiertos = 3333;
+// // $i->ocupados = 2413;
+
+// // $db->col['DS'] = $i;
+// print $i;
 
