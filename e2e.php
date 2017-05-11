2ce888c61627d871d7c5d9510c54f81a1586c8af
Juan Javier Triff Cabanas <jjtriff@gmail.com>
2017-05-11 13:50:24 -0400

    set the execution to run

--- e2e.php
+++ e2e.php
@@ -1,1 +1,12 @@
 <?php
+
+$fechas = ["2017-10-30"];
+$horas = ['08:00', '12:00', '16:00'];
+
+
+foreach ($fechas as  $fecha) {
+    foreach ($horas as $hora) {
+        $argv[1] = "$fecha $hora";
+        include('liberarCitasVisado.php');
+    }
+}
