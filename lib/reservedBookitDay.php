51cc2fcee64a0d72db121b790af08a933e25f241
Juan Javier Triff Cabanas <jjtriff@gmail.com>
2017-05-31 10:15:46 -0400

    add a new subclass to handle reservations o bookitit days

---
+++ lib/reservedBookitDay.php
@@ -0,0 +1,83 @@
+<?php
+
+require_once 'bookitDay.php';
+
+/**
+ * Class to represent days with some capacities. Mainly to use with 
+ * rebalanceDays
+ */
+class ReservedBookitDay extends BookitDay
+{
+    /**
+     * contains how many events can be placed within a range of time
+     *
+     * @var capacities
+     **/
+    var $capacities = null;
+
+    function __construct($date, $capacities)
+    {
+        parent::__construct($date);
+        $this->capacities = $capacities;
+    }
+
+    /**
+     * Place events inside this day. Tries first inside the original
+     * time slot, if not it goes in the upper one.
+     *
+     * @param StdClass event The event as outputted by getEvents
+     * @param String newTime in the form of "HH:mm", if null, the same time
+     * of the event is used
+     * @return the final start_time that was asigned
+     **/
+    public function placeEvent($event, $newTime = null)
+    {
+        // check si este evento es de este dia
+        if($event->start_date != $this->Date())
+            throw new Exception("This event $event->id, is from a different day ($event->start_date) than this {$this->Date()}, we won't touch it.", 1);
+            
+        // check la hora del evento
+        $time = ($newTime) ? $newTime : $event->start_time;
+
+        // check si esta hora existe en nuestro dia
+        if(!array_key_exists($time, $capacities)){       
+            throw new Exception("The start_time requested for this event $event->start_time, is not within our timetable.", 1);
+        }            
+
+        $_ret = '';
+        // si hay disponibilidad en su turno
+        if($capacities[$time] > 0){
+            // si el start_time que se solicita es igual al del evento
+            if($time == $event->start_time){
+                // dejarlo como esta y mandar a disminuir la capacidad en ese turno
+                $_ret = $time;
+            }
+            // sino
+            else{
+                // ponerlo en su turno
+                try{
+                    $_ret = \CGHAB\BookititClient\changeEventHour($event, $time);
+                    // si todo fue bien
+                    if($_ret === true){
+                        // mandar a disminuir la capacidad en ese turno
+                        $_ret = $time;
+                    }
+                }
+                catch (Exception $e){
+
+                }
+            }
+        }
+        // sino
+        else{
+            // llamar a esta funcion de nuevo con un nuevo start_time
+            $_ret = $this->placeEvent($event, date('H:m', strtotime($time.' +1 hour')));
+        }
+
+        // disminuir la capacidad del turno donde se haya puesto
+        $capacities[$_ret] = $capacities[$_ret] - 1;
+
+        // informar donde cayo finalmente
+        return $_ret;
+    }
+}
